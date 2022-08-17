1f4d96e895d1751fd2d8e88ff19ff7f971026fa3
Arsen <Arcenk3x@mail.ru>
2022-08-17 17:55:39 +0500

    footer added

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,3 +4,15 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+	anchor.addEventListener("click", function (evt) {
+		event.preventDefault();
+		const blockID = anchor.getAttribute('href')
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: "smooth",
+			block: "start"
+		})
+	})
+}
